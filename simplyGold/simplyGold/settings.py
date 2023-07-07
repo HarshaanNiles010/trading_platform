ee60690fea81746b95301d4d1a90ebb19a720e1a
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-07-07 14:11:20 -0700

    I forgot what I added

--- simplyGold/simplyGold/settings.py
+++ simplyGold/simplyGold/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'simplyGold.wsgi.application'
-
+#ASGI_APPLICATION = 'simplyGold.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
