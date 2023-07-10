ed415e4767ade9b7ac48470908bade8a5c87700c
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-07-10 13:03:32 -0700

    working on login

--- simplyGold/users/views.py
+++ simplyGold/users/views.py
@@ -7,4 +7,11 @@ def index(request):
     pass
 
 def login_users(request):
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST.get('username'),password=request.POST.get('password'))
+        if user is not None:
+            login(request,user)
+            return render(request,'authentication/success.html',{'name':"meh"})
+        else:
+            return render(request,'authentication/fail.html',{'name':"ehhh"})
     return render(request,'authentication/login.html',{})
