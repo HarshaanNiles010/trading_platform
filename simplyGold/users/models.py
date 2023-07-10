5f1540a88c76f2dee6d5817b3662bcef8e3e843e
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-07-10 14:26:18 -0700

    user authentication working

--- simplyGold/users/models.py
+++ simplyGold/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    fist_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200,default=f'john.doe@gmail.com')
--- simplyGold/users/views.py
+++ simplyGold/users/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     pass
 
 def login_users(request):
+    
     if request.method == "POST":
         user = authenticate(request, username=request.POST.get('username'),password=request.POST.get('password'))
         if user is not None:
             login(request,user)
             return render(request,'authentication/success.html',{'name':"meh"})
         else:
-            return render(request,'authentication/fail.html',{'name':"ehhh"})
+            new_email = f"{request.POST.get('username')}@example.com"
+            new_user = User.objects.create_user(request.POST.get('username'),new_email,request.POST.get('password'))
+            return render(request,'authentication/fail.html',{'name':"ehhhh"})
     return render(request,'authentication/login.html',{})
