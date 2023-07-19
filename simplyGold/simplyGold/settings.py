b7446196b92f804440af276d91d54c3e1ec620b9
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-07-19 15:32:29 -0700

    Working on payment gateway

--- simplyGold/simplyGold/settings.py
+++ simplyGold/simplyGold/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os 
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
