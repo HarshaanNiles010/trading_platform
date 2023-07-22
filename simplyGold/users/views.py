a62c8e21476f6854c764193f36f0aefc7c4a773d
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-07-21 17:11:39 -0700

    making small changes

--- simplyGold/users/views.py
+++ simplyGold/users/views.py
@@ -11,16 +11,16 @@ from django.contrib import messages
 def index(request):
     pass
 
- def login_users(request):
+def login_users(request):
     
-     if request.method == "POST":
-         user = authenticate(request, username=request.POST.get('username'),password=request.POST.get('password'))
-         if user is not None:
-             login(request,user)
-             return render(request,'authentication/success.html',{'name':"meh"})
-         else:
-             new_email = f"{request.POST.get('username')}@example.com"
-             new_user = User.objects.create_user(request.POST.get('username'),new_email,request.POST.get('password'))
-             return render(request,'authentication/fail.html',{'name':"ehhhh"})
-     return render(request,'authentication/login.html',{})
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST.get('username'),password=request.POST.get('password'))
+        if user is not None:
+            login(request,user)
+            return render(request,'authentication/success.html',{'name':"meh"})
+        else:
+            new_email = f"{request.POST.get('username')}@example.com"
+            new_user = User.objects.create_user(request.POST.get('username'),new_email,request.POST.get('password'))
+            return render(request,'authentication/fail.html',{'name':"ehhhh"})
+    return render(request,'authentication/login.html',{})
 
